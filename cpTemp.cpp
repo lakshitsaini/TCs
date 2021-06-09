ca31137dccb51bb10245c082e3235168a16804dd
Lakshit Saini <lakshitsaini2017@gmail.com>
2021-06-09 07:57:02 +0530

    Create cpTemp.cpp

---
+++ cpTemp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>  // This will work only for g++ compiler. 
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i) // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i) // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i) // closed interver from l to r r inclusive
+#define forr0(i, n) for (int i = (int)(n) - 1; i >= 0; --i) // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i) // reverse 1 based
+#define pb push_back
+#define fi first
+#define se second
+#define sz(a) int((a).size())
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(10);
+    cout << fixed;
+    
+    return 0;
+}
